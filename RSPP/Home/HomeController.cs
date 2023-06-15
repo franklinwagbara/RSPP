d50b6806d2d8062978e71549abf6782bf31414a9
emmanuel Okpukpan <emmaist23@gmail.com>
2023-06-15 08:06:21 +0100

    feat(logging): added log configurations using log4net

--- RSPP/Home/HomeController.cs
+++ RSPP/Home/HomeController.cs
@@ -1,11 +1,13 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 using Rotativa.AspNetCore;
 using RSPP.Configurations;
 using RSPP.Models;
 using RSPP.Models.DB;
 using System;
+using System.Diagnostics;
 using System.Linq;
 
 namespace RSPP.Home
@@ -17,9 +19,15 @@ namespace RSPP.Home
         public IConfiguration _configuration;
         HelperController _helpersController;
         IHttpContextAccessor _httpContextAccessor;
+        private readonly ILogger<HomeController> _logger;
 
-        public HomeController(RSPPdbContext context, IConfiguration configuration, IHttpContextAccessor httpContextAccessor)
+        public HomeController(
+            RSPPdbContext context, 
+            IConfiguration configuration, 
+            IHttpContextAccessor httpContextAccessor,
+            ILogger<HomeController> logger)
         {
+            _logger = logger;
             _context = context;
             _configuration = configuration;
             _httpContextAccessor = httpContextAccessor;
@@ -27,6 +35,9 @@ namespace RSPP.Home
         }
         public IActionResult Index()
         {
+            //_logger.LogInformation("Index() called");
+            //var rng = new Random();
+            //_logger.LogWarning("Random object was created");
             return View();
         }
 
@@ -84,6 +95,16 @@ namespace RSPP.Home
             }
 
             return View("VerifyCertificate", certificate);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+
+            var data = HttpContext.Request.Headers["4o4Path"];
+            _logger.LogError(data);
+
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
