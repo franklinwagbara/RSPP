f2b481de09063026198d7478f0def276ae37cf61
emmanuel Okpukpan <emmaist23@gmail.com>
2023-06-16 11:02:38 +0100

    feat(login): added models for authentication

---
+++ RSPP/Models/DTOs/AuthenticationResponse.cs
@@ -0,0 +1,18 @@
+﻿namespace RSPP.Models.DTOs
+{
+    public class AuthenticationResponse: BasicResponse
+    {
+        public AuthenticationResponse() : base(false, null)
+        {
+
+        }
+
+        public AuthenticationResponse(bool success, string? message) : base(success, message)
+        {
+
+        }
+
+        public string UserType { get; set; }
+        public bool IsEmailConfirmed { get; set; }
+    }
+}
