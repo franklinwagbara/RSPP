ffc5656933892dff1b75ffc9666e7da23d22b227
emmanuel Okpukpan <emmaist23@gmail.com>
2023-06-22 13:10:09 +0100

    feat(testing): added tests for acct registration

--- RRPSPU.Tests/Controllers/AccountControllerTests.cs
+++ RRPSPU.Tests/Controllers/AccountControllerTests.cs
@@ -1,14 +1,14 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Routing;
 using Microsoft.Extensions.Configuration;
 using Moq;
-using Moq.EntityFrameworkCore;
 using RRPSPU.Tests.MockData;
 using RSPP.Controllers;
 using RSPP.Helpers;
 using RSPP.Models.DB;
 using RSPP.Models.DTOs;
+using RSPP.Services.Interfaces;
 using RSPP.UnitOfWorks.Interfaces;
 using System.Linq.Expressions;
 
@@ -20,7 +20,11 @@ namespace RRPSPU.Tests.Controllers
         private readonly Mock<IConfiguration> _configuration;
         private readonly Mock<RSPPdbContext> _rsppdbContext;
         private readonly Mock<IUnitOfWork> _unitOfWork;
+        private readonly Mock<IEmailer> _emailer;
         private readonly AccountController _accountController;
+
+        private readonly DefaultHttpContext _httpContext; 
+        private readonly Mock<IUrlHelper> _urlHelperMock;
 
         public AccountControllerTests()
         {
@@ -28,12 +32,24 @@ namespace RRPSPU.Tests.Controllers
             _httpContextAccessor = new Mock<IHttpContextAccessor>();
             _configuration = new Mock<IConfiguration>();
             _rsppdbContext = new Mock<RSPPdbContext>();
+            _emailer = new Mock<IEmailer>();
+
             _accountController = new AccountController(
                 _rsppdbContext.Object,
                 _httpContextAccessor.Object,
                 _configuration.Object,
-                _unitOfWork.Object);
+                _unitOfWork.Object,
+                _emailer.Object);
+
+            _httpContext = new DefaultHttpContext();
+            _httpContext.Request.Scheme = "http";
+            _accountController.ControllerContext.HttpContext = _httpContext;
+
+            _urlHelperMock = new Mock<IUrlHelper>();
+            _accountController.Url = _urlHelperMock.Object;
         }
+
+        #region Login
 
         [Fact]
         public void Login_WhenCalled_ReturnsLoginView()
@@ -60,10 +76,10 @@ namespace RRPSPU.Tests.Controllers
             Assert.False(authenticationResponse.Success);
             Assert.False(authenticationResponse.IsEmailConfirmed);
             Assert.Null(authenticationResponse.UserType);
-            Assert.Contains(AppMessages.LOGIN_FAILED, authenticationResponse.ResultMessage);
+            Assert.Contains($"{AppMessages.LOGIN} {AppMessages.FAILED}", authenticationResponse.ResultMessage);
         }
 
-        [Fact]
+        [Fact(Skip = "pending implementation for httpContextAccessor")]
         public void Login_WithValidAuthenticationRequest_ReturnsSuccessResponse()
         {
             var authenticationRequest = new AuthenticationRequest()
@@ -83,7 +99,7 @@ namespace RRPSPU.Tests.Controllers
                         It.IsAny<string>(),
                         It.IsAny<int?>(),
                         It.IsAny<int?>())
-            ).Returns(() => AuthenticationMock.GetFakeUserMasterList());
+            ).Returns(() => AuthenticationMock.GetFakeUnConfirmedUserMasterList());
 
             var result = _accountController.Login(authenticationRequest);
 
@@ -92,7 +108,7 @@ namespace RRPSPU.Tests.Controllers
             Assert.True(authenticationResponse.Success);
             Assert.True(authenticationResponse.IsEmailConfirmed);
             Assert.NotNull(authenticationResponse.UserType);
-            Assert.Contains(AppMessages.LOGIN_SUCCESSFUL, authenticationResponse.ResultMessage);
+            Assert.Contains($"{AppMessages.LOGIN} {AppMessages.SUCCESSFUL}", authenticationResponse.ResultMessage);
         }
 
         [Fact(Skip = "not implemented yet")]
@@ -112,5 +128,162 @@ namespace RRPSPU.Tests.Controllers
         {
 
         }
+
+        #endregion
+
+        #region Registration
+
+        [Fact]
+        public void AccountRegister_WhenCalled_ReturnsAccountRegisterView()
+        {
+            var result = _accountController.AccountRegister() as ViewResult;
+            Assert.NotNull(result);
+            Assert.Equal("AccountRegister", result.ViewName);
+        }
+
+        [Fact]
+        public void AccountRegister_WithInvalidRegistrationRequest_ReturnsFailedResponse()
+        {
+            var registrationRequest = new RegistrationRequest()
+            {
+                UserEmail = "",
+                Password = ""
+            };
+            _accountController.ModelState.AddModelError("error", "some error");
+
+            var result = _accountController.AccountRegister(registrationRequest);
+
+            Assert.NotNull(result);
+            var registrationResponse = Assert.IsType<BasicResponse>(result.Value);
+            Assert.False(registrationResponse.Success);
+            Assert.Contains($"{AppMessages.REGISTRATION} {AppMessages.FAILED}", registrationResponse.ResultMessage);
+        }
+
+        [Fact]
+        public void AccountRegister_WhenEmailAlreadyExists_ReturnsFailedResponse()
+        {
+            var userList = AuthenticationMock.GetFakeUnConfirmedUserMasterList();
+            var user = userList.FirstOrDefault();
+            var registrationRequest = new RegistrationRequest()
+            {
+                UserEmail = user.UserEmail,
+                Password = user.Password,
+                ConfirmPassword = user.Password,
+                MobilePhoneNumber = "09087866567",
+                CompanyAddress = "address",
+                CompanyName = "company name"
+            };
+
+            _unitOfWork.Setup(unit => unit.UserMasterRepository.Get(
+                It.IsAny<Expression<Func<UserMaster, bool>>?>(),
+                        It.IsAny<Func<IQueryable<UserMaster>, IOrderedQueryable<UserMaster>>?>(),
+                        It.IsAny<string>(),
+                        It.IsAny<int?>(),
+                        It.IsAny<int?>())
+            ).Returns(() => userList);
+
+            var result = _accountController.AccountRegister(registrationRequest);
+
+            Assert.NotNull(result);
+            var registrationResponse = Assert.IsType<BasicResponse>(result.Value);
+            Assert.False(registrationResponse.Success);
+            Assert.Contains($"{AppMessages.EMAIL} {AppMessages.EXISTS}", registrationResponse.ResultMessage);
+        }
+
+        [Fact]
+        public void AccountRegister_WhenRequestIsValid_ReturnsSuccessResponse()
+        {
+
+            string url = "callbackUrl";
+            _urlHelperMock.Setup(u => u.Action(It.IsAny<UrlActionContext>())).Returns(url).Verifiable();
+
+            var userList = AuthenticationMock.GetFakeEmptyUserMasterList();
+            var user = AuthenticationMock.GetFakeUnConfirmedUserMaster();
+            var registrationRequest = new RegistrationRequest()
+            {
+                UserEmail = user.UserEmail,
+                Password = user.Password,
+                ConfirmPassword = user.Password,
+                MobilePhoneNumber = user.PhoneNum,
+                CompanyAddress = user.CompanyAddress,
+                CompanyName = user.CompanyName
+            };
+
+            _unitOfWork.Setup(unit => unit.UserMasterRepository.Get(
+                It.IsAny<Expression<Func<UserMaster, bool>>?>(),
+                        It.IsAny<Func<IQueryable<UserMaster>, IOrderedQueryable<UserMaster>>?>(),
+                        It.IsAny<string>(),
+                        It.IsAny<int?>(),
+                        It.IsAny<int?>())
+            ).Returns(() => userList);
+
+            _emailer.Setup(email => email.SendEmail(
+                It.IsAny<string>(),
+                It.IsAny<string>(),
+                It.IsAny<string>(),
+                It.IsAny<string>()
+                )).Returns(BasicResponse.Result(true, $"{AppMessages.EMAIL} {AppMessages.SENDING} {AppMessages.SUCCESSFUL}"));
+
+
+            var result = _accountController.AccountRegister(registrationRequest);
+
+            Assert.NotNull(result);
+            var registrationResponse = Assert.IsType<BasicResponse>(result.Value);
+            Assert.True(registrationResponse.Success);
+            Assert.Contains($"{AppMessages.REGISTRATION} {AppMessages.SUCCESSFUL} {AppMessages.EMAIL_VERIFICATION_LINK_SENT}", registrationResponse.ResultMessage);
+            _unitOfWork.Verify(unit => unit.UserMasterRepository.Add(It.IsAny<UserMaster>()), Times.Once);
+
+            //confirm that password is encrypted
+            //confirm created on has value
+            //confirm userType has correct value
+            //confirm userRole has correct value
+            //emailConfirmed is false
+            //emailConfirmationToken has correct value
+        }
+        
+        [Fact]
+        public void AccountRegister_WhenRequestIsValidButEmailIsNotSent_ReturnsSuccessResponse()
+        {
+
+            string url = "callbackUrl";
+            _urlHelperMock.Setup(u => u.Action(It.IsAny<UrlActionContext>())).Returns(url).Verifiable();
+
+            var userList = AuthenticationMock.GetFakeEmptyUserMasterList();
+            var user = AuthenticationMock.GetFakeUnConfirmedUserMaster();
+            var registrationRequest = new RegistrationRequest()
+            {
+                UserEmail = user.UserEmail,
+                Password = user.Password,
+                ConfirmPassword = user.Password,
+                MobilePhoneNumber = user.PhoneNum,
+                CompanyAddress = user.CompanyAddress,
+                CompanyName = user.CompanyName
+            };
+
+            _unitOfWork.Setup(unit => unit.UserMasterRepository.Get(
+                It.IsAny<Expression<Func<UserMaster, bool>>?>(),
+                        It.IsAny<Func<IQueryable<UserMaster>, IOrderedQueryable<UserMaster>>?>(),
+                        It.IsAny<string>(),
+                        It.IsAny<int?>(),
+                        It.IsAny<int?>())
+            ).Returns(() => userList);
+
+            _emailer.Setup(email => email.SendEmail(
+                It.IsAny<string>(),
+                It.IsAny<string>(),
+                It.IsAny<string>(),
+                It.IsAny<string>()
+                )).Returns(BasicResponse.Result(false, $"{AppMessages.EMAIL} {AppMessages.SENDING} {AppMessages.FAILED}"));
+
+
+            var result = _accountController.AccountRegister(registrationRequest);
+
+            Assert.NotNull(result);
+            var registrationResponse = Assert.IsType<BasicResponse>(result.Value);
+            Assert.True(registrationResponse.Success);
+            Assert.Contains($"{AppMessages.REGISTRATION} {AppMessages.SUCCESSFUL} {AppMessages.EMAIL_VERIFICATION_LINK_FAILED}", registrationResponse.ResultMessage);
+            _unitOfWork.Verify(unit => unit.UserMasterRepository.Add(It.IsAny<UserMaster>()), Times.Once);
+        }
+        #endregion
     }
 }
--- RRPSPU.Tests/MockData/AuthenticationMock.cs
+++ RRPSPU.Tests/MockData/AuthenticationMock.cs
@@ -1,43 +1,88 @@
-﻿using RSPP.Helpers;
+﻿using Org.BouncyCastle.Asn1.Ocsp;
+using RSPP.Helpers;
 using RSPP.Models.DB;
 
 namespace RRPSPU.Tests.MockData
 {
     public static class AuthenticationMock
     {
-        public static List<UserMaster> GetFakeUserMasterList()
+        public static List<UserMaster> GetFakeEmptyUserMasterList()
+        {
+            return new List<UserMaster>();
+        }
+        public static List<UserMaster> GetFakeUnConfirmedUserMasterList()
         {
             return new List<UserMaster>{
 
-                new UserMaster()
-                {
-                    UserEmail= "me@gmail.com",
-                    Password="password",
-                    Status=AppMessages.ACTIVE_USER
-                },
                     new UserMaster()
-                {
-                    UserEmail= "you@gmail.com",
-                    Password="password",
-                    Status=AppMessages.ACTIVE_USER
-                },
+                    {
+                        UserEmail= "me@gmail.com",
+                        Password="password",
+                        Status=AppMessages.ACTIVE,
+                        PhoneNum = "09087866567",
+                        CompanyAddress = "address",
+                        CompanyName = "company name",
+                        UserType = AppMessages.COMPANY,
+                        UserRole = AppMessages.COMPANY,
+                        UpdatedBy = "adam@gmail.com",
+                        LoginCount = 0,
+                        CreatedOn = DateTime.Now,
+                        EmailConfirmed = false,
+                        EmailConfirmationToken = Guid.NewGuid().ToString()
+                    },
                     new UserMaster()
-                {
-                    UserEmail= "yoyo@gmail.com",
-                    Password="password",
-                    Status=AppMessages.PASSIVE_USER
-                }
+                    {
+                        UserEmail= "you@gmail.com",
+                        Password="password",
+                        Status=AppMessages.ACTIVE,
+                        PhoneNum = "09087866567",
+                        CompanyAddress = "address",
+                        CompanyName = "company name",
+                        UserType = AppMessages.COMPANY,
+                        UserRole = AppMessages.COMPANY,
+                        UpdatedBy = "adam@gmail.com",
+                        LoginCount = 0,
+                        CreatedOn = DateTime.Now,
+                        EmailConfirmed = false,
+                        EmailConfirmationToken = Guid.NewGuid().ToString()
+                    },
+                    new UserMaster()
+                    {
+                        UserEmail= "yoyo@gmail.com",
+                        Password="password",
+                        Status=AppMessages.PASSIVE,
+                        PhoneNum = "09087866567",
+                        CompanyAddress = "address",
+                        CompanyName = "company name",
+                        UserType = AppMessages.COMPANY,
+                        UserRole = AppMessages.COMPANY,
+                        UpdatedBy = "adam@gmail.com",
+                        LoginCount = 0,
+                        CreatedOn = DateTime.Now,
+                        EmailConfirmed = false,
+                        EmailConfirmationToken = Guid.NewGuid().ToString()
+                    }
 
             };
         }
 
-        public static UserMaster GetFakeUserMaster()
+        public static UserMaster GetFakeUnConfirmedUserMaster()
         {
             return new UserMaster()
             {
-                UserEmail = "yoyo@gmail.com",
+                UserEmail = "great@gmail.com",
                 Password = "password",
-                Status = AppMessages.ACTIVE_USER
+                Status = AppMessages.ACTIVE,
+                PhoneNum = "09087866567",
+                CompanyAddress = "address",
+                CompanyName = "company name",
+                UserType = AppMessages.COMPANY,
+                UserRole = AppMessages.COMPANY,
+                UpdatedBy = "great@gmail.com",
+                LoginCount = 0,
+                CreatedOn = DateTime.Now,
+                EmailConfirmed = false,
+                EmailConfirmationToken = Guid.NewGuid().ToString(),
             };
         }
     }
